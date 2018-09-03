ffa56f7ef0da1dd468d1a142277f677b8c607e0b
Alfred Law <alfred.lawsn@gmail.com>
2018-09-03 21:04:34 +0000

    added code to invoke getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,23 @@
-var request = require('request');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-
-
 // Github API
 // https://api.github.com
 
 // access token:
 // 8e5e59947a26ba3a327874837b86c71a22df38a1
+
+
+
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
