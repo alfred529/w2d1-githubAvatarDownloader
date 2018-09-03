1d19bc14cd4bf402db270d3c822cda844ea13194
Alfred Law <alfred.lawsn@gmail.com>
2018-09-03 21:01:32 +0000

    created download_avatars.js file

---
+++ download_avatars.js
@@ -0,0 +1,11 @@
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
+// Github API
+// https://api.github.com
+
+// access token:
+// 8e5e59947a26ba3a327874837b86c71a22df38a1
